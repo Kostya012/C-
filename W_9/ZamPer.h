a18d22b080fa323bf32b18b9334a882cc92d2c1d
Kostya012 <61709439+Kostya012@users.noreply.github.com>
2020-09-28 13:17:33 +0300

    ZamPer

---
+++ W_9/ZamPer.h
@@ -0,0 +1,26 @@
+//---------------------------------------------------------------------------
+
+#ifndef ZamPerH
+#define ZamPerH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Buttons.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TBitBtn *Pysk;
+        TBitBtn *Zamena;
+        void __fastcall PyskClick(TObject *Sender);
+        void __fastcall ZamenaClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
---
+++ W_9/ZamPer.~dfm
@@ -0,0 +1,34 @@
+object Form1: TForm1
+  Left = 192
+  Top = 107
+  Width = 696
+  Height = 480
+  Caption = #1042#1089#1090#1072#1074#1082#1072' '#1090#1077#1082#1089#1090#1072' '#1089' '#1079#1072#1084#1077#1085#1086#1081
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Pysk: TBitBtn
+    Left = 24
+    Top = 40
+    Width = 113
+    Height = 33
+    Caption = '&'#1055#1091#1089#1082
+    TabOrder = 0
+    OnClick = PyskClick
+  end
+  object Zamena: TBitBtn
+    Left = 160
+    Top = 40
+    Width = 113
+    Height = 33
+    Caption = #1047#1072#1084#1077#1085#1072
+    TabOrder = 1
+    OnClick = ZamenaClick
+  end
+end
