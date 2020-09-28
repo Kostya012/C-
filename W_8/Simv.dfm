90aeb833e467acabd0fe43493e8ba34d3f320957
Kostya012 <61709439+Kostya012@users.noreply.github.com>
2020-09-28 13:16:50 +0300

    Simv

---
+++ W_8/Simv.dfm
@@ -0,0 +1,61 @@
+object Form1: TForm1
+  Left = 192
+  Top = 107
+  Width = 696
+  Height = 480
+  Caption = 'Символьные переменные'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object BitBtn1: TBitBtn
+    Left = 32
+    Top = 24
+    Width = 81
+    Height = 25
+    Caption = 'ю'
+    TabOrder = 0
+    OnClick = BitBtn1Click
+  end
+  object BitBtn2: TBitBtn
+    Left = 136
+    Top = 24
+    Width = 89
+    Height = 25
+    Caption = 'Ю'
+    TabOrder = 1
+    OnClick = BitBtn2Click
+  end
+  object BitBtn3: TBitBtn
+    Left = 256
+    Top = 24
+    Width = 97
+    Height = 25
+    Caption = 'Ykaz'
+    TabOrder = 2
+    OnClick = BitBtn3Click
+  end
+  object BitBtn4: TBitBtn
+    Left = 376
+    Top = 24
+    Width = 81
+    Height = 25
+    Caption = 'S_tring'
+    TabOrder = 3
+    OnClick = BitBtn4Click
+  end
+  object BitBtn5: TBitBtn
+    Left = 480
+    Top = 24
+    Width = 97
+    Height = 25
+    Caption = 'Конкатинация'
+    TabOrder = 4
+    OnClick = BitBtn5Click
+  end
+end
---
+++ W_8/Simv.h
@@ -0,0 +1,32 @@
+//---------------------------------------------------------------------------
+
+#ifndef SimvH
+#define SimvH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <Buttons.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TBitBtn *BitBtn1;
+        TBitBtn *BitBtn2;
+        TBitBtn *BitBtn3;
+        TBitBtn *BitBtn4;
+        TBitBtn *BitBtn5;
+        void __fastcall BitBtn1Click(TObject *Sender);
+        void __fastcall BitBtn2Click(TObject *Sender);
+        void __fastcall BitBtn3Click(TObject *Sender);
+        void __fastcall BitBtn4Click(TObject *Sender);
+        void __fastcall BitBtn5Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
